eaacac5a043ec7006e06907f2db12df3de5365ad
Vebjørn Kveberg Opsanger <kvebops@gmail.com>
2020-10-07 14:00:52 +0200

    explored data and tried to apply models

---
+++ Data exploration.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(readxl)
+
+df <- read_excel("data/Pollen data.xlsx")
+
+Pollen_data <- df %>%
+  mutate(
+    Tree = substring(Sample, 0, 3),   # Create column for tree number
+    Frame = substring(Sample, 5, 5),   # Create column for frame number
+    Treatment_full = substring(Sample, 7, 8), # Create column for treatment type with number
+    Treatment = substring(Sample, 7, 7) # Create column for treatment type 
+  )
+
+c_tetrad <- Pollen_data %>% #Data from control
+  filter(Treatment == 'C')
+
+t_data <- Pollen_data %>% #Data from UV-B treatment        
+  filter(Treatment == 'T') 
+
+# Scatterplots
+
+ggplot(Pollen_data, aes(Tetrad, Num_malform,)) +   #Both control and UV-B treatment in one
+  geom_point(aes(shape = Treatment)) +
+  geom_abline()
+
+ggplot(Pollen_data, aes(Tetrad, Num_malform,)) +   #Facet wrap of control and UV-B treatment
+  geom_point(aes(shape = Treatment)) +
+  geom_abline() +
+  facet_wrap(~ Treatment, nrow=1)
+
+ggplot(Pollen_data, aes(Num_malform, Tetrad)) +
+  geom_point() +
+  geom_abline() +
+  facet_wrap(~ Tree, nrow = 2)
+
+# Correlation tests
+
+cor.test(Pollen_data$Num_malform, Pollen_data$Tetrad, #All data
+         alternative = "greater", 
+         method = "kendall")
+
+cor.test(t_data$Num_malform, t_data$Tetrad, #Only control data
+         alternative = "greater",
+         method = "kendall")
+
+cor.test(c_data$Num_malform, c_data$Tetrad, # Only treatment data
+         alternative = "greater", 
+         method = "kendall")
+
+  
+#Boxplots
+
+ggplot(Pollen_data ,aes(Treatment, Tetrad, group = Treatment)) +#Comparison of tetrads
+  geom_boxplot()
+
+ggplot(Pollen_data ,aes(Treatment, Num_malform, group = Treatment)) +  #Comparison of malformations
+  geom_boxplot()
+
+ggplot(Pollen_data ,aes(Frame, Num_malform, group = Frame)) + # Comparison between frames with regards to number of malformations
+  geom_boxplot()
+
+ggplot(Pollen_data ,aes(Frame, Tetrad, group = Frame)) + # Comparison of frames in regards to tetrads
+  geom_boxplot()
+
---
+++ Models.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(readxl)
+library(MASS)
+df <- read_excel("data/Pollen data.xlsx")
+
+Pollen_data <- df %>%
+  mutate(
+    Tree = substring(Sample, 0, 3),   # Create column for tree number
+    Frame = substring(Sample, 5, 5),   # Create column for frame number
+    Treatment_full = substring(Sample, 7, 8), # Create column for treatment type with number
+    Treatment = substring(Sample, 7, 7)
+  )
+
+c_data <- Pollen_data %>% #Data from control
+  filter(Treatment == 'C')
+
+t_data <- Pollen_data %>% #Data from UV-B treatment        
+  filter(Treatment == 'T')
+
+
+linear_mod <-lm(Tetrad ~ Num_malform, Pollen_data)
+summary(linear_mod)
+
+linear_mod_t <-lm(Num_malform ~ Tetrad, t_data)
+summary(linear_mod_t)
+
+linear_mod_c <- lm(Num_malform ~ Tetrad, c_data)
+summary(linear_mod_c)
+
+view(t_data)
+view(c_data)

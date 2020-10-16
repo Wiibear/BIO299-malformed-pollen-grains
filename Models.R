0f51d85362c0bd6ae98d0a690283edeb2d1b9896
alistairseddon <alistair.seddon@gmail.com>
2020-10-16 11:19:53 +0200

    Alistair fixed code. Fit a few different models using glmer and lme. The glmer ones are more technically correct, but it seems like you get the same results whatever

--- Models.R
+++ Models.R
@@ -3,6 +3,8 @@ library(readxl)
 library(nlme)
 df <- read_excel("data/Pollen data.xlsx")
 
+dfNoOut <- filter(df, Num_malform < 90 & Tetrad < 60)
+
 Pollen_data <- df %>%
   filter(Num_malform < 90)
 
@@ -11,14 +13,15 @@ Pollen_data <- dfNoOut %>%
     Tree = substring(Sample, 0, 3),   # Create column for tree number
     Frame = substring(Sample, 5, 5),   # Create column for frame number
     code = substring(Sample, 7, 8),
-    Malformation_rate = (Num_malform/600),
-  ) %>%
+    Malformation_rate = (Num_malform/600)) %>%
   filter(code == "C4" | code == "T2") %>%
   rename(
     treatment = code      #Didn't manage to make column with treatment as UV or C
                           #I tried this ---> treatment = if(Frame == "1"|Frame == "3"|Frame == "5") {"C"} else {"UV"}
   )                       
 
+# Alistair comment. This model looks correct to me!  Except perhaps we should be using a generalised liner model instead because ther residuals of the malformation rate cannot be less than zero 
+# (i.e. they are non-normally distributed)
 model1<-lme(Malformation_rate ~ treatment,
             random = ~1 |Tree, data = Pollen_data)
 summary(model1)
@@ -27,6 +30,23 @@ residuals = resid(model1)
 qqnorm(residuals)              #Checking normal distribution of residuals
 qqline(residuals)
 
+# Although maybe we can specify the correct error distribution. 
+# I think instead I would should use a binomial error distribution. 
+# But this means we need to use a different package to fit the model. 
+# I have never used glmer before but I think this is correct model specification
+
+install.packages("lme4")
+library(lme4)
+model2<-glmer(cbind(Num_malform, 600- Num_malform) ~ treatment + (1 | Tree),
+             data = Pollen_data, family = binomial(link= "logit"))
+summary(model2)
+
+# Or modelling them as count data
+model3<-glmer( Num_malform ~ treatment + (1 | Tree),
+              data = Pollen_data, family = poisson(link= "log"))
+summary(model3)
+
+
 ###
 #Experimentation
 meanMalfFrame <- Pollen_data %>%
@@ -34,25 +54,16 @@ meanMalfFrame <- Pollen_data %>%
   summarize(meanMalform = mean(Num_malform))%>%
   mutate(
     treatment = if(Frame == "1"|Frame == "3"|Frame == "5") {"C"} else {"UV"}
-  )
+  ) %>% 
+  mutate(meanMalform = ceiling(meanMalform))
 view(meanMalfFrame)
 
-meanMalfTree <- Pollen_data %>%
-  group_by(Tree, Frame, code) %>%
-  summarize(treeMeanMalf = mean(Num_malform)) %>%
-  mutate(
-    treatment = if(Frame == "1"|Frame == "3"|Frame == "5") {"C"} else {"UV"}
-  )
-
-m1Tree<-lme(treeMeanMalf ~ treatment,random = ~1 |Tree, data = meanMalfTree)
+# Here I corrected it, so you have a glm and poisson error distribution. You don't need the random effects if you specify it in this way
+m1Tree<-glm(meanMalform ~ treatment, data = meanMalfFrame, family = poisson(link = "log"))
 summary(m1Tree)
 
-residTree = resid(m1Tree)
-qqnorm(residTree)              #Checking normal distribution of residuals
-qqline(residTree)
 
-
-
+### I'll leave this for you to play with yourself
 
 model1Tet<-lme(Tetrad ~ treatment,
             random = ~1 |Tree, data = Pollen_data)
